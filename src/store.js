6e3416eee8ccbcd057222449a4e635c3ec7d880c
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-05-22 03:29:03 +0300

    feat: store configuration for Redux

---
+++ src/store.js
@@ -0,0 +1,28 @@
+/* eslint-disable linebreak-style */
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from './reducers';
+
+export const middlewares = [thunk];
+const persistConfig = {
+  key: 'root',
+  storage,
+  timeout: 0,
+  stateReconciler: autoMergeLevel2,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+const persistor = persistStore(store);
+
+export { store, persistor };
