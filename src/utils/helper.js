5a1b80612b079ec7b6058e563a7db8acb1e56b75
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-05-22 03:34:05 +0300

    feat: helper functions

---
+++ src/utils/helper.js
@@ -0,0 +1,30 @@
+/* eslint-disable linebreak-style */
+export const timeConverter = (timeStamp) => {
+  if (!timeConverter.cache) {
+    timeConverter.cache = {};
+  }
+
+  if (!timeConverter.cache[timeStamp]) {
+    const timeStampMs = timeStamp * 1000;
+    const dateObj = new Date(timeStampMs);
+    const dayOfMonth = dateObj.getDate() < 10 ? `0${dateObj.getDate()}` : dateObj.getDate();
+    const months = [
+      'January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December',
+    ];
+    const curMonth = months[dateObj.getMonth()];
+    const curYear = dateObj.getFullYear();
+    const curMinute = dateObj.getMinutes() < 10 ? `0${dateObj.getMinutes()}` : dateObj.getMinutes();
+    const curHour =
+          dateObj.getHours() > 12 ?
+            dateObj.getHours() - 12 :
+            (dateObj.getHours() < 10 ? `0${dateObj.getHours()}` : dateObj.getHours());
+
+    timeConverter.cache[timeStamp] = {
+      date: `${curMonth} ${dayOfMonth},${curYear}`,
+      timed: `${curHour}:${curMinute}`,
+    };
+    return timeConverter.cache[timeStamp];
+  }
+  return timeConverter.cache[timeStamp];
+};
