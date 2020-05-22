a288931ebb32b87db7b75ee99cd94b1c5b82dc12
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-05-22 03:57:20 +0300

    feat: margin & position updates

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -7,7 +7,7 @@ export const Wrapper = styled.div`
   height: ${(props) =>
     props.incomingIcon || props.incomingAvatar || props.sendIcon ? '40px' : '20px'};
   display: ${(props) => (props.scrollDown && props.appearance ? 'none' : 'flex')};
-  position: ${(props) => (props.sendIcon ? 'absolute' : props.scrollDown ? 'fixed' : 'relative')};
+  position: ${(props) => (props.sendIcon || props.scrollDown ? 'absolute' : 'relative')};
   border-radius: 50%;
   justify-content: center;
   align-items: center;
@@ -15,7 +15,7 @@ export const Wrapper = styled.div`
   background-color: ${(props) => (props.incomingIcon ? ' #fff' : '#00bcd4')};
   border: ${(props) => (props.incomingIcon ? '1px solid #00bcd4' : 'none')};
   cursor: ${(props) => (props.scrollDown ? 'pointer' : 'auto')};
-  margin: ${(props) => (props.scrollDown ? '0' : '0 7px 0 10px')};
+  margin: ${(props) => (props.scrollDown ? '0 7px 0 0' : '0 7px 0 10px')};
   right: ${(props) => (props.sendIcon || props.scrollDown ? '0' : 'inherit')};
   bottom: ${(props) => (props.sendIcon ? '10px' : 'inherit')};
   box-shadow: 0.1em 0.1em rgba(0, 0, 0, 0.12);
