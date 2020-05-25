80737ca39aa5022946a1494a323366d8a48c9f9f
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-05-26 00:56:11 +0300

    feat: icon view changed

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -4,13 +4,15 @@ import { Wrapper } from '../Wrapper/Wrapper';
 
 export const Icon = styled.svg`
   fill: currentColor;
-  width: ${(props) => (props.sendIcon ? '1.5em' : '1em')};
-  height: ${(props) => (props.sendIcon ? '1.5em' : '1em')};
+  width: ${(props) => (props.sendIcon ? '1.5em' : '2em')};
+  height: ${(props) => (props.sendIcon ? '1.5em' : '2em')};
   font-size: ${(props) => (props.incomingAvatar ? '1.5rem' : '1rem')};
   color: ${(props) => (props.incomingIcon ? '#00bcd4' : '#fff')};
   margin-right: ${(props) => (props.incomingIcon ? '0.1rem' : 0)};
   position: ${(props) => (props.sendIcon ? 'absolute' : 'relative')};
-  transform: ${(props) => (props.scrollDown ? 'rotate(-90deg)' : 0)};
+  transform: ${(props) => (props.scrollDown ? 'rotate(90deg)' :
+    props.incomingIcon ? 'rotate(180deg)' :
+      'rotate(0)')};
   cursor: ${(props) => (props.sendIcon || props.scrollDown ? 'pointer' : 'auto')};
    ${Wrapper}:hover & {
     color: #fff
