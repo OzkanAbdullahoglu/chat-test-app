629cd59090f36ba0ddfb670324bf4da4ad63e32d
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-05-29 05:57:16 +0300

    Updates

---
+++ precache-manifest.5ea268edd5e413aa571cacca37f4080e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d717fbbf168499c699a86394214df17",
+    "url": "/chat-test-app/index.html"
+  },
+  {
+    "revision": "c78e42092fb7dc2050dc",
+    "url": "/chat-test-app/static/css/main.8f3d9c39.chunk.css"
+  },
+  {
+    "revision": "408e321a21ed819f2f72",
+    "url": "/chat-test-app/static/js/2.ba52e248.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/chat-test-app/static/js/2.ba52e248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78e42092fb7dc2050dc",
+    "url": "/chat-test-app/static/js/main.470cd19f.chunk.js"
+  },
+  {
+    "revision": "df85b36079415a4ececd",
+    "url": "/chat-test-app/static/js/runtime-main.7d51b7a5.js"
+  },
+  {
+    "revision": "123c7e674188c97798dd39cd41ff2d85",
+    "url": "/chat-test-app/static/media/Brown-Regular.123c7e67.ttf"
+  },
+  {
+    "revision": "704cfe8e61a8f488020f294175449a80",
+    "url": "/chat-test-app/static/media/background.704cfe8e.jpg"
+  }
+]);
