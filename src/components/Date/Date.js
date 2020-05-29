e44d7d12a88496dfa1b3e22f07009f30bb25a73f
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-05-29 05:20:35 +0300

    feat: date formatted changed

--- src/components/Date/Date.js
+++ src/components/Date/Date.js
@@ -2,12 +2,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Date.css';
+import { isToday } from '../../utils/helper';
 const Date = ({
   pack,
 }) => (
   <div className="list-item-date-root">
-    <div className="list-item-date-text-primary timestamp">
-      <span>{pack.date}</span>
+    <div
+      className="list-item-date-text-primary timestamp"
+      aria-label="Date"
+    >
+      <span>{isToday(pack.timestamp, pack.date)}</span>
     </div>
   </div>
 );
