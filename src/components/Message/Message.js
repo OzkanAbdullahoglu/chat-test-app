8fafb299c347da3cf70c337a98fb8275e75dfcc1
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-06-21 22:04:54 +0300

    fix: get rid of inline style

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -5,51 +5,39 @@ import PropTypes from 'prop-types';
 import './Message.css';
 const Message = ({
   pack,
-}) => (
-  <div className="list-item-root">
-    <div className="list-item-text-root">
-      {pack.direction === 'out' ? (
-        <div className="list-item-text-primary message-out">
-          <span data-test="item-text">{pack.text}</span>
-          <span
-            className="time-inline"
-          >{pack.time}</span>
-          {pack.status === 'sent' ? (
+}) => {
+  const IconStyle = {
+    fontSize: '1rem',
+    padding: 0,
+    float: 'right',
+    alignSelf: 'center',
+    color: pack.status === 'read' ? 'blue' : '#777',
+  };
+  return (
+    <div className="list-item-root">
+      <div className="list-item-text-root">
+        {pack.direction === 'out' ? (
+          <div className="list-item-text-primary message-out">
+            <span data-test="item-text">{pack.text}</span>
+            <span
+              className="time-inline"
+            >{pack.time}</span>
             <Icon
-              style={{
-                fontSize: '1rem',
-                color: 'gray',
-                padding: 0,
-                float: 'right',
-                alignSelf: 'center',
-              }}
-              data-test="sent-icon"
-            >done</Icon>
-          ) : (
-            <Icon
-              style={{
-                fontSize: '1rem',
-                padding: 0,
-                float: 'right',
-                alignSelf: 'center',
-                color: pack.status === 'read' ? 'blue' : '#777',
-              }}
-              data-test="read-icon"
-            >
-            done_all
-            </Icon>
-          )}
-        </div>
-      ) : (
-        <div className="list-item-text-primary">
-          <span data-test="item-text">{pack.text}</span>
-          <span className="time-inline">{pack.time}</span>
-        </div>
-      )
-      }
+              style={IconStyle}
+              data-test={pack.status === 'sent' ? 'sent-icon' : 'read-icon'}
+            >{pack.status === 'sent' ? 'done' : 'done_all'}</Icon>
+          </div>
+        ) : (
+          <div className="list-item-text-primary">
+            <span data-test="item-text">{pack.text}</span>
+            <span className="time-inline">{pack.time}</span>
+          </div>
+        )
+        }
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Message.propTypes = {
   pack: PropTypes.object,
