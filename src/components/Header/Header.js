d79f0b09a877966e33d9deea2043563e468f0036
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2020-06-21 22:04:25 +0300

    fix: refactoring

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ import {
   chatActions, getUnreadMessages,
 } from '../../reducers';
 import { Wrapper } from '../Wrapper/Wrapper';
-import SvgIcon from '../Icon/Icon';
+import IconContainer from '../IconContainer/IconContainer';
 
 const Header = ({
   unreadMessages,
@@ -16,68 +16,40 @@ const Header = ({
   incomingAvatar,
 }) => (
   <div className="container-root">
-    {incomingAvatar ? (
-      <div className="header-root incoming-user">
-        <Wrapper
-          data-test="wrapper"
-          incomingIcon
-          aria-label="Back to chat screen"
-          tabIndex="0"
-        >
-          <SvgIcon
-            incomingIcon
-            viewBox="0 0 48 48"
-          >
-            <path xmlns="http://www.w3.org/2000/svg" d="M20 12l-2.83 2.83 9.17 9.17-9.17 9.17 2.83 2.83 12-12z" />
-            <path xmlns="http://www.w3.org/2000/svg" d="M0 0h48v48h-48z" fill="none" />
-          </SvgIcon>
-        </Wrapper>
+    <div className={incomingAvatar ? 'header-root incoming-user' : 'header-root'}>
+      <Wrapper
+        data-test="wrapper"
+        incomingIcon={incomingAvatar ? incomingIcon : false}
+        aria-label={incomingAvatar ? 'Back to chat screen' : 'View User 1 profile'}
+        tabIndex="0"
+      >
+        <IconContainer
+          incomingIcon={incomingAvatar ? incomingIcon : false}
+          arrow={!!incomingAvatar}
+          scrollDown={false}
+          viewBox={incomingAvatar ? '0 0 48 48' : '0 0 24 24'}
+        />
+      </Wrapper>
+      {incomingAvatar ? (
         <Wrapper
           incomingAvatar
           aria-label="View User 2 profile"
           tabIndex="0"
         >
-          <SvgIcon
-            incomingAvatar
+          <IconContainer
+            incomingAvatar={incomingAvatar}
+            arrow={false}
+            scrollDown={false}
             viewBox="0 0 24 24"
-          >
-            <path
-              d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
-            />
-          </SvgIcon>
+          />
         </Wrapper>
-        <div className="header-content" tabIndex="0">
-          <span
-            className="header-text"
-          >User 2</span>
-        </div>
+      ) : null }
+      <div className="header-content" tabIndex="0">
+        <span
+          className="header-text"
+        >{incomingAvatar ? 'User 2' : `User 1 (${unreadMessages.count} new messages)`}</span>
       </div>
-    ) : (
-      <div className="header-root" >
-        <Wrapper
-          data-test="wrapper"
-          incomingAvatar={false}
-          aria-label="View User 1 profile"
-          tabIndex="0"
-        >
-          <SvgIcon
-            incomingAvatar={false}
-            viewBox="0 0 24 24"
-            data-test="icon"
-          >
-            <path
-              d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
-            />
-          </SvgIcon>
-        </Wrapper>
-        <div className="header-content" tabIndex="0">
-          <span
-            className="header-text"
-          >User 1 ({unreadMessages.count} new messages)</span>
-        </div>
-      </div>
-    )
-    }
+    </div>
   </div>
 );
 
